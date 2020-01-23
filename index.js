4dcd0200e8ffb748f5235d155fe4c99205d68814
Azeez Olasoji <azeezolasoji@gmail.com>
2020-01-23 20:29:56 +0100

    Finished the code along, NS - include preprocessor LESS & make it respnsive

--- index.js
+++ index.js
@@ -10,16 +10,34 @@ const openHTML = ()=>{
     content1.style.transform = "translateX(0)";
     content2.style.transform = "translateX(100%)";
     content3.style.transform = "translateX(100%)";
+    btn1.style.color = "#ff7846"
+    btn2.style.color = "#000"
+    btn3.style.color = "#000"
+    content1.style.transitionDelay = "0.3s"
+    content2.style.transitionDelay = "0s"
+    content3.style.transitionDelay = "0s"
 }
 
 const openCSS = ()=>{
     content1.style.transform = "translateX(100%)";
     content2.style.transform = "translateX(0)";
     content3.style.transform = "translateX(100%)";
+    btn1.style.color = "#000"
+    btn2.style.color = "#ff7846"
+    btn3.style.color = "#000"
+    content1.style.transitionDelay = "0s"
+    content2.style.transitionDelay = "0.3s"
+    content3.style.transitionDelay = "0s"
 }
 
 const openJS = ()=>{
     content1.style.transform = "translateX(100%)";
     content2.style.transform = "translateX(100%)";
     content3.style.transform = "translateX(0)";
+    btn1.style.color = "#000"
+    btn2.style.color = "#000"
+    btn3.style.color = "#ff7846"
+    content1.style.transitionDelay = "0s"
+    content2.style.transitionDelay = "0s"
+    content3.style.transitionDelay = "0.3s"
 }
